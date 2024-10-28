bcf631bcf612f8a186d3b1b43cb90406c0387873
miguelfrndz <miguel.fretana@gmail.com>
2024-10-28 21:10:58 +0100

    Changed to MNIST dataset

--- generateData.py
+++ generateData.py
@@ -1,12 +1,18 @@
 import os
 import numpy as np
-from sklearn.datasets import load_breast_cancer
+from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+
+NORMALIZE = False
 
 # If data folder does not exist, create it
 if not os.path.exists('data'):
     os.makedirs('data')
+
+def preprocess(img):
+    thresh = img.mean()
+    binary = img > thresh
+    return binary.astype(int)
 
 # Function to save data to a txt file
 def save_to_txt(filename, X, y):
@@ -20,21 +26,17 @@ def save_to_txt(filename, X, y):
             f.write(f"{features} {y[i]}\n")
 
 # Load the breast cancer dataset
-data = load_breast_cancer()
+data = load_digits(n_class = 2)
 X, y = data.data, data.target
 
-# Split the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# Preprocess the data
+X = np.array([preprocess(x.reshape(8, 8)).flatten() for x in X])
 
-# Normalize the training set
-scaler = StandardScaler()
-X_train_normalized = scaler.fit_transform(X_train)
-
-# Normalize the testing set using the same scaler
-X_test_normalized = scaler.transform(X_test)
+# Split the dataset into training and testing sets (stratified)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42, stratify = y)
 
 # Save the train and test sets to txt files
-save_to_txt('data/train.txt', X_train_normalized, y_train)
-save_to_txt('data/test.txt', X_test_normalized, y_test)
+save_to_txt('data/train.txt', X_train, y_train)
+save_to_txt('data/test.txt', X_test, y_test)
 
 print("Data saved successfully!")
